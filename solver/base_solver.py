314dfc3b629e7afebe6ed733a23a5377de3c34cd
PrinceSJ <sj86033503@163.com>
2021-05-13 22:14:08 +0800

    add yaml config files for office-home

--- solver/base_solver.py
+++ solver/base_solver.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 import os
-from . import utils as solver_utils 
+from . import utils as solver_utils
 from utils.utils import to_cuda, mean_accuracy, accuracy
 from torch import optim
 from math import ceil as ceil
 from config.config import cfg
+
 
 class BaseSolver:
     def __init__(self, net, dataloader, bn_domain_map={}, resume=None, **kwargs):
@@ -18,7 +19,7 @@ class BaseSolver:
 
         self.CELoss = nn.CrossEntropyLoss()
         if torch.cuda.is_available():
-            self.CELoss.cuda() 
+            self.CELoss.cuda()
 
         self.loop = 0
         self.iters = 0
@@ -39,8 +40,8 @@ class BaseSolver:
             self.history = resume['history']
             self.optim_state_dict = resume['optimizer_state_dict']
             self.bn_domain_map = resume['bn_domain_map']
-            print('Resume Training from loop %d, iters %d.' % \
-			(self.loop, self.iters))
+            print('Resume Training from loop %d, iters %d.' %
+                  (self.loop, self.iters))
 
         self.build_optimizer()
 
@@ -50,30 +51,30 @@ class BaseSolver:
             if key not in dataloader:
                 continue
             cur_dataloader = dataloader[key]
-            self.train_data[key]['loader'] = cur_dataloader 
+            self.train_data[key]['loader'] = cur_dataloader
             self.train_data[key]['iterator'] = None
 
         if 'test' in dataloader:
             self.test_data = dict()
             self.test_data['loader'] = dataloader['test']
-        
+
     def build_optimizer(self):
         opt = self.opt
-        param_groups = solver_utils.set_param_groups(self.net, 
-		dict({'FC': opt.TRAIN.LR_MULT}))
+        param_groups = solver_utils.set_param_groups(self.net,
+                                                     dict({'FC': opt.TRAIN.LR_MULT}))
 
         assert opt.TRAIN.OPTIMIZER in ["Adam", "SGD"], \
             "Currently do not support your specified optimizer."
 
         if opt.TRAIN.OPTIMIZER == "Adam":
-            self.optimizer = optim.Adam(param_groups, 
-			lr=self.base_lr, betas=[opt.ADAM.BETA1, opt.ADAM.BETA2], 
-			weight_decay=opt.TRAIN.WEIGHT_DECAY)
+            self.optimizer = optim.Adam(param_groups,
+                                        lr=self.base_lr, betas=[opt.ADAM.BETA1, opt.ADAM.BETA2],
+                                        weight_decay=opt.TRAIN.WEIGHT_DECAY)
 
         elif opt.TRAIN.OPTIMIZER == "SGD":
-            self.optimizer = optim.SGD(param_groups, 
-			lr=self.base_lr, momentum=self.momentum, 
-			weight_decay=opt.TRAIN.WEIGHT_DECAY)
+            self.optimizer = optim.SGD(param_groups,
+                                       lr=self.base_lr, momentum=self.momentum,
+                                       weight_decay=opt.TRAIN.WEIGHT_DECAY)
 
         if self.optim_state_dict is not None:
             self.optimizer.load_state_dict(self.optim_state_dict)
@@ -81,14 +82,14 @@ class BaseSolver:
     def update_lr(self):
         iters = self.iters
         if self.opt.TRAIN.LR_SCHEDULE == 'exp':
-            solver_utils.adjust_learning_rate_exp(self.base_lr, 
-			self.optimizer, iters, 
-                        decay_rate=self.opt.EXP.LR_DECAY_RATE,
-			decay_step=self.opt.EXP.LR_DECAY_STEP)
+            solver_utils.adjust_learning_rate_exp(self.base_lr,
+                                                  self.optimizer, iters,
+                                                  decay_rate=self.opt.EXP.LR_DECAY_RATE,
+                                                  decay_step=self.opt.EXP.LR_DECAY_STEP)
 
         elif self.opt.TRAIN.LR_SCHEDULE == 'inv':
-            solver_utils.adjust_learning_rate_inv(self.base_lr, self.optimizer, 
-		    iters, self.opt.INV.ALPHA, self.opt.INV.BETA)
+            solver_utils.adjust_learning_rate_inv(self.base_lr, self.optimizer,
+                                                  iters, self.opt.INV.ALPHA, self.opt.INV.BETA)
 
         else:
             raise NotImplementedError("Currently don't support the specified \
@@ -103,15 +104,15 @@ class BaseSolver:
             loss_values += '%.4f,' % (loss[key])
         loss_names = loss_names[:-1] + ': '
         loss_values = loss_values[:-1] + ';'
-        loss_str = loss_names + loss_values + (' source %s: %.4f.' % 
-                    (self.opt.EVAL_METRIC, accu))
+        loss_str = loss_names + loss_values + (' source %s: %.4f.' %
+                                               (self.opt.EVAL_METRIC, accu))
         print(loss_str)
 
     def model_eval(self, preds, gts):
         assert(self.opt.EVAL_METRIC in ['mean_accu', 'accuracy']), \
-             "Currently don't support the evaluation metric you specified."
+            "Currently don't support the evaluation metric you specified."
 
-        if self.opt.EVAL_METRIC == "mean_accu": 
+        if self.opt.EVAL_METRIC == "mean_accu":
             res = mean_accuracy(preds, gts)
         elif self.opt.EVAL_METRIC == "accuracy":
             res = accuracy(preds, gts)
@@ -142,23 +143,23 @@ class BaseSolver:
             self.history[key] = [value]
         else:
             self.history[key] += [value]
-        
+
         if len(self.history[key]) > history_len:
             self.history[key] = \
-                 self.history[key][len(self.history[key]) - history_len:]
-       
+                self.history[key][len(self.history[key]) - history_len:]
+
     def solve(self):
         print('Training Done!')
 
     def get_samples(self, data_name):
         assert(data_name in self.train_data)
-        assert('loader' in self.train_data[data_name] and \
+        assert('loader' in self.train_data[data_name] and
                'iterator' in self.train_data[data_name])
 
         data_loader = self.train_data[data_name]['loader']
         data_iterator = self.train_data[data_name]['iterator']
         assert data_loader is not None and data_iterator is not None, \
-            'Check your dataloader of %s.' % data_name 
+            'Check your dataloader of %s.' % data_name
 
         try:
             sample = next(data_iterator)
@@ -170,7 +171,7 @@ class BaseSolver:
 
     def get_samples_categorical(self, data_name, category):
         assert(data_name in self.train_data)
-        assert('loader' in self.train_data[data_name] and \
+        assert('loader' in self.train_data[data_name] and
                'iterator' in self.train_data[data_name])
 
         data_loader = self.train_data[data_name]['loader'][category]
@@ -212,4 +213,3 @@ class BaseSolver:
 
     def update_network(self, **kwargs):
         pass
-
